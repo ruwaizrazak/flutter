3e99543193de9ac1b3e50dd2984726af9df47390
Ruwaiz <ruwaizrazak@gmail.com>
2020-03-19 19:12:21 +0530

    Initial commit

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//main function is the starting point for all the flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
